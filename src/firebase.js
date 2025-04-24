71167c728e7e839aad779a350fb89da9de240c08
opinivisual <136211840+opinivisual@users.noreply.github.com>
2025-04-25 04:06:55 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyEXAMPLE1234567890",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdefghij"
+  apiKey: "AIzaSyDqC2OcqbBTWGGI6Qn46KNg8kMQ9_dRVeE",
+  authDomain: "kumien-b2486.firebaseapp.com",
+  projectId: "kumien-b2486",
+  storageBucket: "kumien-b2486.firebasestorage.app",
+  messagingSenderId: "872654517059",
+  appId: "1:872654517059:web:9408a7b3c621aee53e75f6",
+  measurementId: "G-4N3ZSFP60D"
 };
 
 const app = initializeApp(firebaseConfig);
